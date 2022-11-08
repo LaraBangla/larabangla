9df03f7a62401aa55c74c12e7a75ab32341e15ea
Anowar Hosen <megamindjony@gmail.com>
2022-11-08 15:46:45 +0600

    dashboard sidebar

--- resources/views/backend/backend_header_footer.blade.php
+++ resources/views/backend/backend_header_footer.blade.php
@@ -37,9 +37,66 @@
           }"
     >
         <div class="flex">
-            <div class=" w-60 h-screen bg-slate-200 border-r border-gray-300 duration-500" x-show="sidebar" x-transition>
+            <div class=" w-96 md:w-60 h-screen bg-slate-200 border-r border-gray-300 duration-500" x-show="sidebar" x-transition>
                 <div class="text-center pt-6 pb-6 border-b border-gray-300">
-                    <a class=" text-gray-600 text-xl font-bold" href="#">Dashboard</a>
+                    <a href="{{ route('dashboard') }}" class=" text-gray-600 text-xl font-bold" href="#">Dashboard</a>
+                </div>
+                <div>
+                    <div class=" leading-7 cursor-pointer">
+                        <ul class=" text-gray-700 pl-2">
+                            <li class=" border-b  border-gray-300 pt-4 pb-1 mb-2">
+                                <div class=" flex items-center mb-2">
+                                    <div>
+                                        <img class=" rounded-full w-8 h-8 ring-2 ring-slate-400" src="https://ui-avatars.com/api/?name=Anowar+Hosen&background=0000&color=fff" alt="avtar">
+                                    </div>
+                                    <div class=" pl-2">
+                                        <a href="#" class=" font-medium">Anowar Hosen</a>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class=" hover:bg-gray-300 pl-2 rounded font-semibold"><a href="{{ route('/') }}" ><span><i class="fa-solid fa-globe"></i></span> Visite website</a></li>
+                            <li class="mt-1" x-data="{open: false}" >
+                                <div class=" hover:bg-gray-300 pl-2 rounded"  @click="open = ! open">
+                                    <a  class=" font-semibold"><span><i class="fa-regular fa-folder"></i></span> Category <span><i class="fa-solid fa-caret-down"></i></span></a>
+                                </div>
+
+                                <ul x-show="open" x-collapse.duration.300ms>
+                                    <li class="pl-3 rounded hover:bg-gray-300 font-medium"><a href="#"><span><i class="fa-regular fa-circle"></i></span> Add Category</a></li>
+                                    <li class="pl-3 rounded hover:bg-gray-300 font-medium"><a href="#"><span><i class="fa-regular fa-circle"></i></span> All Category</a></li>
+                                </ul>
+                            </li>
+                            <li class="mt-1" x-data="{open: false}" >
+                                <div class=" hover:bg-gray-300 pl-2 rounded"  @click="open = ! open">
+                                    <a class=" font-semibold" ><span><i class="fa-regular fa-folder"></i></span> Category <span><i class="fa-solid fa-caret-down"></i></span></a>
+                                </div>
+
+                                <ul x-show="open" x-collapse.duration.300ms>
+                                    <li class="pl-3 rounded hover:bg-gray-300 font-medium"><a href="#"><span><i class="fa-regular fa-circle"></i></span> Add Category</a></li>
+                                    <li class="pl-3 rounded hover:bg-gray-300 font-medium"><a href="#"><span><i class="fa-regular fa-circle"></i></span> All Category</a></li>
+                                </ul>
+                            </li>
+                            <li class="mt-1" x-data="{open: false}" >
+                                <div class=" hover:bg-gray-300 pl-2 rounded"  @click="open = ! open">
+                                    <a class=" font-semibold" ><span><i class="fa-regular fa-folder"></i></span> Category <span><i class="fa-solid fa-caret-down"></i></span></a>
+                                </div>
+
+                                <ul x-show="open" x-collapse.duration.300ms>
+                                    <li class="pl-3 rounded hover:bg-gray-300 font-medium"><a href="#"><span><i class="fa-regular fa-circle"></i></span> Add Category</a></li>
+                                    <li class="pl-3 rounded hover:bg-gray-300 font-medium"><a href="#"><span><i class="fa-regular fa-circle"></i></span> All Category</a></li>
+                                </ul>
+                            </li>
+                            <li class="mt-1" x-data="{open: false}" >
+                                <div class=" hover:bg-gray-300 pl-2 rounded"  @click="open = ! open">
+                                    <a class=" font-semibold" ><span><i class="fa-regular fa-folder"></i></span> Category <span><i class="fa-solid fa-caret-down"></i></span></a>
+                                </div>
+
+                                <ul x-show="open" x-collapse.duration.300ms>
+                                    <li class="pl-3 rounded hover:bg-gray-300 font-medium"><a href="#"><span><i class="fa-regular fa-circle"></i></span> Add Category</a></li>
+                                    <li class="pl-3 rounded hover:bg-gray-300 font-medium"><a href="#"><span><i class="fa-regular fa-circle"></i></span> All Category</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
             <div class=" w-screen">
@@ -48,7 +105,7 @@
                         <li class="px-3 py-2 ml-8 " @click="sidebar =! sidebar" ><span><span class=" text-2xl text-gray-800"><i class="fa-solid fa-bars"></i></span></span></li>
                         <li class=" grow"></li>
                         <li class="px-3 mr-1 py-2 rounded-full"><a href="#"><span class=" text-xl"><i class="fa-regular fa-bell"></i></span> </a><span class=" mt-2 -ml-2 bg-yellow-500 text-gray-800 w-5 h-5 text-center absolute text-sm rounded-full">1</span></li>
-                        <li class="px-3 py-1 mr-2"><img class=" rounded-full w-8 ring-2" src="https://ui-avatars.com/api/?name=Anowar+Hosen&background=0000&color=fff" alt=""></li>
+                        <li class="px-3 py-1 mr-2"><img class=" rounded-full w-8 ring-2 ring-slate-400" src="https://ui-avatars.com/api/?name=Anowar+Hosen&background=0000&color=fff" alt=""></li>
                     </ul>
                 </div>
             </div>
