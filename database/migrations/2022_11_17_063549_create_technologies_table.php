b3e09e957fb14defef13d104470943083b9d4876
Anowar Hosen <megamindjony@gmail.com>
2022-11-17 12:38:42 +0600

    technology,version,chapter,lesson migration added

--- database/migrations/2022_11_17_063549_create_technologies_table.php
+++ database/migrations/2022_11_17_063549_create_technologies_table.php
@@ -14,13 +14,11 @@ return new class extends Migration
     public function up()
     {
         Schema::create('technologies', function (Blueprint $table) {
-            Schema::create('technologies', function (Blueprint $table) {
-                $table->id();
-                $table->string('name');
-                $table->string('slug')->unique;
-                $table->integer('status')->default(1);
-                $table->timestamps();
-            });
+            $table->id();
+            $table->string('name');
+            $table->string('slug')->unique;
+            $table->integer('status')->default(1);
+            $table->timestamps();
         });
     }
 
