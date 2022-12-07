1701c78b8a1211795c0b253264118c22875814cf
Anowar Hosen <megamindjony@gmail.com>
2022-12-07 10:56:39 +0600

    empty lesson hide from frontend Tutorial dropdown menu

--- resources/views/frontend/more/doc_sidebar.blade.php
+++ resources/views/frontend/more/doc_sidebar.blade.php
@@ -1,6 +1,7 @@
 <li class="pt-3 pl-1 text-base font-semibold text-gray-600"><a href="{{ route('/') }}"><span class="text-xl text-gray-500"><i
          class="fa fa-home mr-4"></i></span>হোম</a></li>
 @foreach ($chapters as $chapter)
+
   @if (count($chapter->lessons) > 0)
     <li class="mt-5 mb-2 border-b pb-1 font-bold text-gray-700">{{ $chapter->name }}</li>
   @endif
