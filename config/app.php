67ff83acc10f9e65dee3e0a392903128c09917ea
Anowar Hosen <megamindjony@gmail.com>
2022-12-26 11:21:19 +0600

    bangla-number system

--- config/app.php
+++ config/app.php
@@ -181,6 +181,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Rakibhstu\Banglanumber\NumberToBanglaServiceProvider::class,
 
         /*
          * Package Service Providers...
