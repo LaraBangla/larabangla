cae2281bbc837685f1505dba31a02e91cd904bbd
Anowar Hosen <megamindjony@gmail.com>
2023-01-04 13:38:36 +0600

    homepage left side bar mergin fix

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -85,15 +85,15 @@
       </div>
  {{-- /subcription --}}
       <div>
-          <div class="md:flex justify-around -ml-4 -mb-10">
-            <div>
-             <div class="  mr-5 float-left h-full" style="width: 3px; background: linear-gradient(#d2a8ff, #a371f7 10%, #196c2e 70%, #2ea043 80%, #56d364);"></div>
-             <div class=" float-right py-16 ">
+          <div class="md:flex block md:justify-around 2xl:-ml-5 xl:-ml-10 lg:-ml-16 mx-10 md:mx-0">
+            <div class="mx-auto md:mx-0">
+             <div class="  md:mr-5 md:float-left h-full lg:block md:hidden" style="width: 3px; background: linear-gradient(#d2a8ff, #a371f7 10%, #196c2e 70%, #2ea043 80%, #56d364);"></div>
+             <div class=" md:float-right py-16 ">
                <p class="  font-black text-3xl text-green-500">আপনি কি শিক্ষানবিস?</p>
                <p class=" mt-4 text-white text-lg"> নতুন টেকনলজি শিখতে চাচ্ছেন? লারা বাংলা আপনার সাথেই আছে।</p>
              </div>
             </div>
-            <div class=" md:w-1/3 sm:w-screen  py-16 ">
+            <div class=" md:w-1/3 sm:w-full  py-16 -mt-24 md:mt-0">
       <div class="code-toolbar"><pre class="language-php" tabindex="0"><code class="language-php">composer <span class="token keyword">global</span> <span class="token keyword">require</span> laravel<span class="token operator">/</span>installer 
 laravel <span class="token keyword">new</span> <span class="token class-name">example</span><span class="token operator">-</span>app</code></pre><div class="toolbar"><div class="toolbar-item"><button class="copy-to-clipboard-button" type="button" data-copy-state="copy"><span>Copy</span></button></div></div></div>
             </div>
@@ -102,57 +102,63 @@ laravel <span class="token keyword">new</span> <span class="token class-name">ex
 
  
 
-      <div class=" -ml-8 ">
+      <div class="-mt-14 md:mt-0 md:-ml-8 ">
         <div>
-          <div class="flex justify-around ">
-            <div class=" mb-10 mt-5">
-              <div>
+          <div class="md:flex  md:justify-around ">
+
+            <div class=" md:mb-10 mt-5 ">
+              <div class="lg:block hidden">
                 <div class="-ml-3 text-xl text-white" >
                   <span ><i class="fa-solid fa-layer-group mt-10 mb-5" ></i></span>
                 </div>
               </div>
-             <div class="mr-5 float-left h-full" style="width: 3px; background: linear-gradient(#d2a8ff, #a371f7 10%, #196c2e 70%, #2ea043 80%, #56d364);"></div>
-             <div class=" float-right py-16">
-              <img src="{{ asset('img/css.png') }}" style="max-height: 700px;" alt="Code">
+
+             <div class="mr-5 md:float-left h-full lg:block hidden" style="width: 3px; background: linear-gradient(#d2a8ff, #a371f7 10%, #196c2e 70%, #2ea043 80%, #56d364);"></div>
+             <div class=" md:float-right py-16 ">
+              <img src="{{ asset('img/css.png') }}" class="block mx-auto md:mx-0 w-full md:w-auto" style="max-height: 700px;" alt="Code">
              </div>
             </div>
-            <div class=" pb-16 pt-4 flex justify-center items-center">
+
+            <div class=" pb-16 md:pt-4 flex justify-center items-center mx-12 md:mx-0">
              <div class=" max-w-md">
               <p class="  font-black text-3xl text-green-500 ">ওয়েব ডিজাইনে আগ্রহী?</p>  
               <p class=" text-white mt-4 leading-8">আপনি যদি ওয়েব ডিজাইন শিখতে আগ্রহী হন, আপনি সঠিক জায়গায় এসেছেন। এখানে আপনি বিভিন্ন রিসোর্স এবং টিউটোরিয়াল পাবেন যা আপনাকে ওয়েব ডিজাইনের উত্তেজনাপূর্ণ ক্ষেত্রে শুরু করতে সাহায্য করবে। আপনি প্রাথমিক বিষয়গুলি শিখছেন এমন একজন শিক্ষানবিস বা একজন অভিজ্ঞ ডিজাইনার যা আপনার দক্ষতা বাড়াতে চান?, আমাদের সবার জন্য কিছু না কিছু আছে৷ তাই এক কাপ কফি নিন, আরাম করুন, এবং আসুন একসাথে শেখা শুরু করি!</p>
-             </div>
+             
+            </div>
             </div>
           </div>
         </div>
       </div>
 
-      <div class="-ml-5">
+      <div class=" 2xl:-ml-6  lg:-ml-16">
         <div>
-          <div class="flex justify-around ">
-            <div class=" mb-10 mt-5">
-              <div>
+          <div class="md:flex md:justify-around ">
+            <div class=" md:mb-10 mt-5">
+              <div class="lg:block hidden">
                 <div class="-ml-3 text-xl text-white" >
                   <span ><i class="fa-solid fa-layer-group mt-10 mb-5" ></i></span>
                 </div>
               </div>
-             <div class=" float-left h-full" style="width: 3px; background: linear-gradient(#64CE772A 1%, #2ea043 10%, #196c2e 70%, #2ea043 80%, #56d364);"></div>
-             <div class=" float-right pt-16 pb-36">
+             <div class=" md:float-left h-full lg:block hidden" style="width: 3px; background: linear-gradient(#64CE772A 1%, #2ea043 10%, #196c2e 70%, #2ea043 80%, #56d364);"></div>
+             <div class=" md:float-right pt-16 pb-36">
               <div class="flex">
-                <div class="">
+                <div class="lg:block hidden">
                   <img src="{{ asset('img/productivity.svg') }}" alt="svg">
-                 </div>
-                   <div class=" w-96">
-                    <p class="  font-black text-3xl text-green-500">প্রগ্রামিং-এ আগ্রহী?</p>
+                 </div >
+                   <div class=" md:w-96 w-full mx-auto md:mx-0" >
+                    <div class="md:ml-10 sm:mx-24 mx-10 md:mx-0">
+                      <p class="  font-black text-3xl text-green-500">প্রগ্রামিং-এ আগ্রহী?</p>
                    <p class=" mt-4 text-white text-lg leading-8">
                     আপনি কি প্রোগ্রামিং জগতে ডুব দিতে প্রস্তুত? আমরা আপনাকে পথের প্রতিটি ধাপে সাহায্য করতে এখানে আছি। 
                     আমাদের ওয়েবসাইটটি ব্যাপক নির্দেশিকা, টিউটোরিয়াল এবং সংস্থানগুলিতে পূর্ণ যা আপনাকে বিভিন্ন ধরণের প্রোগ্রামিং ভাষা এবং ধারণাগুলিতে গতি আনতে সাহায্য করবে। 
                     তাই আপনার প্রিয় পানীয় নিন এবং আপনার প্রোগ্রামিং যাত্রা শুরু করা যাক!
                    </p>
+                    </div>
                    </div>
               </div>
              </div>
             </div>
-            <div class=" w-1/3 pb-16 pt-40">
+            <div class=" md:w-1/2 lg:w-1/3 w-full pb-16 md:pt-40">
              <img class=" -mt-12" src="{{ asset('img/php-logic-example.png') }}" alt="Code">
           </div>
           </div>
@@ -160,21 +166,21 @@ laravel <span class="token keyword">new</span> <span class="token class-name">ex
       </div>
        
 
-      <div class=" ml-16">
+      <div class=" md:ml-2 2xl:ml-32 xl:ml-26  md:pb-14">
         <div>
-          <div class="flex justify-around ">
-            <div class=" mb-10 mt-5">
-              <div>
+          <div class="md:flex block md:justify-around ">
+            <div class=" md:mb-10 md:mt-5 -mt-14">
+              <div class="lg:block hidden">
                 <div class="-ml-2 text-xl text-white" >
                   <span ><i class="fa-solid fa-layer-group mt-10 mb-5" ></i></span>
                 </div>
               </div>
-             <div class="  mr-5 float-left h-full" style="width: 3px; background: linear-gradient(#d2a8ff, #a371f7 10%, #196c2e 70%, #2ea043 80%, #56d364);"></div>
-             <div class=" float-right py-16">
-              <img src="{{ asset('img/code-2.png') }}" width="800"  alt="Code">
+             <div class="  md:mr-5 md:float-left h-full lg:block hidden" style="width: 3px; background: linear-gradient(#d2a8ff, #a371f7 10%, #196c2e 70%, #2ea043 80%, #56d364);"></div>
+             <div class=" md:float-right py-16 mx-auto" style="max-width: 95%;">
+              <img src="{{ asset('img/code-2.png') }}"   alt="Code">
              </div>
             </div>
-            <div class=" pb-16 pt-4 flex justify-center items-center">
+            <div class="mx-5 md:mx-0 pb-16 pt-4 flex justify-center items-center w-full" >
              <div class=" max-w-md">
               <p class="  font-black text-3xl text-green-500 ">ওয়েব ডেভেলপমেন্টে আগ্রহী?</p>  
               <p class=" text-white mt-4 leading-8">
