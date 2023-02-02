bb8b474ced746c09a95cafd85cbf5410e0795ad3
Anowar Hosen <megamindjony@gmail.com>
2023-02-02 10:50:38 +0600

    sitemap demo blade

---
+++ resources/views/frontend/more/sitemap.blade.php
@@ -0,0 +1,101 @@
+<div id="cont">
+		<ul class="level-0">
+
+            <li class="lhead">https:/ </li>
+            <li><ul class="level-1">
+
+            <li class="lhead">/ </li>
+            <li><ul class="level-2">
+
+            <li class="lhead">www.larabangla.com/  <span class="lcount">19 pages</span></li>
+            
+<li class="lpage"><a href="https://www.larabangla.com/" title="লারা বাংলা - ফ্রি অনলাইন বাংলা ডকুমেন্টেশন এবং টিউটোরিয়াল ওয়েব সাইট">লারা বাংলা - ফ্রি অনলাইন বাংলা ডকুমেন্টেশন এবং টিউটোরিয়াল ওয়েব সাইট</a></li>
+<li class="lpage"><a href="https://www.larabangla.com/about" title="লারা বাংলা - আমাদের সম্পর্কে
+">লারা বাংলা - আমাদের সম্পর্কে
+</a></li>
+<li class="lpage"><a href="https://www.larabangla.com/contact" title="লারা বাংলা - যোগাযোগ ">লারা বাংলা - যোগাযোগ </a></li>
+<li class="lpage"><a href="https://www.larabangla.com/login" title="লারা বাংলা - লগ ইন
+">লারা বাংলা - লগ ইন
+</a></li>
+<li class="lpage"><a href="https://www.larabangla.com/register" title="লারা বাংলা - রেজিস্টার
+">লারা বাংলা - রেজিস্টার
+</a></li>
+<li class="lpage"><a href="https://www.larabangla.com/online-course" title="লারা বাংলা - অনলাইন কোর্স
+">লারা বাংলা - অনলাইন কোর্স
+</a></li>
+<li class="lpage"><a href="https://www.larabangla.com/web-design-and-development" title="লারা বাংলা - ওয়েব ডিজাইন এবং ডেভেলপমেন্ট
+">লারা বাংলা - ওয়েব ডিজাইন এবং ডেভেলপমেন্ট
+</a></li>
+<li class="lpage"><a href="https://www.larabangla.com/apps-development" title="লারা বাংলা - অ্যাপস ডেভেলপমেন্ট
+">লারা বাংলা - অ্যাপস ডেভেলপমেন্ট
+</a></li>
+<li class="lpage"><a href="https://www.larabangla.com/software-development" title="লারা বাংলা - সফটওয়্যার ডেভেলপমেন্ট
+">লারা বাংলা - সফটওয়্যার ডেভেলপমেন্ট
+</a></li>
+<li class="lpage"><a href="https://www.larabangla.com/livewire" title="লারা বাংলা - লাইভওয়্যার
+">লারা বাংলা - লাইভওয়্যার
+</a></li>
+<li class="lpage"><a href="https://www.larabangla.com/blog" title="লারা বাংলা - ব্লগ
+">লারা বাংলা - ব্লগ
+</a></li>
+<li class="lpage"><a href="https://www.larabangla.com/forum" title="লারা বাংলা - ফোরাম
+">লারা বাংলা - ফোরাম
+</a></li>
+<li class="lpage"><a href="https://www.larabangla.com/community" title="লারা বাংলা - কমিউনিটি
+">লারা বাংলা - কমিউনিটি
+</a></li>
+<li class="lpage"><a href="https://www.larabangla.com/question" title="লারা বাংলা - প্রশ্ন
+">লারা বাংলা - প্রশ্ন
+</a></li>
+<li class="lpage"><a href="https://www.larabangla.com/help" title="লারা বাংলা - সাহায্য
+">লারা বাংলা - সাহায্য
+</a></li>
+<li class="lpage"><a href="https://www.larabangla.com/sitemap" title="লারা বাংলা - সাইট ম্যাপ
+">লারা বাংলা - সাইট ম্যাপ
+</a></li>
+<li class="lpage"><a href="https://www.larabangla.com/forgot-password" title="লারা বাংলা - পাসওয়ার্ড ভুলে গেছেন
+">লারা বাংলা - পাসওয়ার্ড ভুলে গেছেন
+</a></li>
+<li class="lpage"><a href="https://www.larabangla.com/terms-of-service" title="লারা বাংলা">লারা বাংলা</a></li>
+<li class="lpage last-page"><a href="https://www.larabangla.com/privacy-policy" title="লারা বাংলা">লারা বাংলা</a></li><li><ul class="level-3">
+
+            <li class="lhead">docs/ </li>
+            <li><ul class="level-4">
+
+            <li class="lhead">laravel/ </li>
+            <li><ul class="level-5 lev5">
+
+            <li class="lhead">9x/ </li>
+            <li><ul class="level-6 lev5">
+
+            <li class="lhead">getting-started/  <span class="lcount">1 pages</span></li>
+            
+<li class="lpage last-page"><a href="https://www.larabangla.com/docs/laravel/9x/getting-started/installation" title="লারাভেল ইন্সটলেশন - Laravel Installation Bangla - লারা বাংলা">লারাভেল ইন্সটলেশন - Laravel Installation Bangla - লারা বাংলা</a></li>
+</ul></li>
+</ul></li>
+</ul><ul class="level-4">
+
+            <li class="lhead">splade/ </li>
+            <li><ul class="level-5 lev5">
+
+            <li class="lhead">1x/ </li>
+            <li><ul class="level-6 lev5">
+
+            <li class="lhead">introduction/  <span class="lcount">2 pages</span></li>
+            
+<li class="lpage"><a href="https://www.larabangla.com/docs/splade/1x/introduction/what-is-laravel-splade" title="লারাভেল স্প্লেড কি? - What is laravel Splade? - লারা বাংলা">লারাভেল স্প্লেড কি? - What is laravel Splade? - লারা বাংলা</a></li>
+<li class="lpage last-page"><a href="https://www.larabangla.com/docs/splade/1x/introduction/automatic-splade-installation" title="স্প্লেড ইনস্টলেশন - Laravel Splade Installation - লারা বাংলা">স্প্লেড ইনস্টলেশন - Laravel Splade Installation - লারা বাংলা</a></li>
+</ul></li>
+</ul></li>
+</ul></li>
+</ul></li>
+</ul></li>
+</ul></li>
+</ul>
+		<!--
+Please note:
+You are not allowed to remove the copyright notice below.
+Thank you!
+www.xml-sitemaps.com
+-->
+	</div>
