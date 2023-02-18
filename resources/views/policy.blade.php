349be27b659f45d063107616c22ca02aaffe675d
Anowar Hosen <megamindjony@gmail.com>
2023-02-18 12:30:35 +0600

    policy added

--- resources/views/policy.blade.php
+++ resources/views/policy.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <div class="pt-4 bg-gray-100">
         <div class="min-h-screen flex flex-col items-center pt-6 sm:pt-0">
             <div>
@@ -10,4 +10,23 @@
             </div>
         </div>
     </div>
-</x-guest-layout>
+</x-guest-layout> --}}
+
+
+@extends('frontend.header_footer')
+@section('title')
+গোপনীয়তা নীতি - লারা বাংলা
+@endsection
+@section('content')
+<section>
+    <div class="container mx-auto md:pb-20 md:pt-10 py-5 ">
+       <div class="mx-3 md:mx-16 lg:mx-40 border rounded px-5 md:px-10 leading-8 py-10 shadow-md">
+        <div>
+        </div>
+            <div class=" content text-justify">
+                {!! $policy !!}
+            </div>
+       </div>
+    </div>
+</section>
+@endsection
