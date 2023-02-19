73d2696798b25ab49d6f3c904a728b2f7b094bc6
Anowar Hosen <megamindjony@gmail.com>
2023-02-19 14:08:39 +0600

    docs route added

--- resources/views/frontend/more/schema-js.blade.php
+++ resources/views/frontend/more/schema-js.blade.php
@@ -1,37 +1,44 @@
 <script type="application/ld+json">
     {
-      "@context": "https://schema.org/", 
-      "@type": "BreadcrumbList", 
+      "@context": "https://schema.org/",
+      "@type": "BreadcrumbList",
       "itemListElement": [{
-        "@type": "ListItem", 
-        "position": 1, 
+        "@type": "ListItem",
+        "position": 1,
         "name": "আমাদের সম্পর্কে",
-        "item": "https://www.larabangla.com/about"  
+        "item": "https://www.larabangla.com/about"
       },{
-        "@type": "ListItem", 
-        "position": 2, 
+        "@type": "ListItem",
+        "position": 2,
         "name": "যোগাযোগ",
-        "item": "https://www.larabangla.com/contact"  
+        "item": "https://www.larabangla.com/contact"
+      },
+      {
+        "@type": "ListItem",
+        "position": 3,
+        "name": "টিউটোরিয়াল সমূহ",
+        "item": "https://www.larabangla.com/tutorials"
+      },
+      {
+        "@type": "ListItem",
+        "position": 4,
+        "name": "লারাভেল টিউটোরিয়াল",
+        "item": "https://www.larabangla.com/laravel"
       },{
-        "@type": "ListItem", 
-        "position": 3, 
-        "name": "লারাভেল ডকুমেন্টেশন/টিউটোরিয়াল",
-        "item": "https://www.larabangla.com/docs/laravel"  
-      },{
-        "@type": "ListItem", 
-        "position": 4, 
+        "@type": "ListItem",
+        "position": 5,
         "name": "স্প্লেড / লারাভেল স্প্লেড",
-        "item": "https://www.larabangla.com/docs/splade"  
+        "item": "https://www.larabangla.com/splade"
       },{
-        "@type": "ListItem", 
-        "position": 5, 
+        "@type": "ListItem",
+        "position": 6,
         "name": "লগ ইন",
-        "item": "https://www.larabangla.com/login"  
+        "item": "https://www.larabangla.com/login"
       },{
-        "@type": "ListItem", 
-        "position": 6, 
+        "@type": "ListItem",
+        "position": 7,
         "name": "রেজিস্টার",
-        "item": "https://www.larabangla.com/register"  
+        "item": "https://www.larabangla.com/register"
       }]
     }
     </script>
