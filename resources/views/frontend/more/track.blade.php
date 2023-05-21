6789416a423e5c510ddd1287825a7958226b304b
Anowar Hosen <85840896+MdAnowarHosen@users.noreply.github.com>
2023-05-21 18:53:35 +0600

    new google adsense code

---
+++ resources/views/frontend/more/track.blade.php
@@ -0,0 +1,18 @@
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-NEQ21J8TWK"></script>
+  {{-- Adsense --}}
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6643057769918459"
+     crossorigin="anonymous"></script>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6643057769918459"
+     crossorigin="anonymous"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-NEQ21J8TWK');
+  </script>
+  {{-- Google adsense code --}}
+  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6643057769918459"
+     crossorigin="anonymous"></script>
+  {{-- Google adsense code end--}}
